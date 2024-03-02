b44aa33403073330350c3122d533653c74cb7505
blue007fff <blue007fff@gmail.com>
2024-03-02 19:25:18 +0900

    modify memeory_alignment.cpp
    test alignas

--- memory_alignment.cpp
+++ memory_alignment.cpp
@@ -97,6 +97,13 @@ void test3()
         alignas(8) int8_t i0[5];
     };
 
+    struct A4
+    {
+        alignas(8) int8_t i0[5];
+        int8_t i1[3];
+        int8_t i2;
+    };
+
     print_type_info<A1>();
     print_member_info(A1, i0);
     std::cout << std::endl;
@@ -108,13 +115,23 @@ void test3()
     print_type_info<A3>();
     print_member_info(A3, i0);
     std::cout << std::endl;
+
+    print_type_info<A4>();
+    print_member_info(A4, i0);
+    print_member_info(A4, i1);
+    print_member_info(A4, i2);
+    std::cout << std::endl;
 }
 
 int main()
 {
-    // https://learn.microsoft.com/ko-kr/cpp/cpp/alignment-cpp-declarations?view=msvc-170#alignof-and-alignas
     // 1. 멤버 변수는 해당 변수의 자료형 크기 배수 위치에서 시작.
     // 2. 객체 크기는 가장 큰 멤버 변수 크기 배수 크기를 가짐.
+    // https://learn.microsoft.com/ko-kr/cpp/cpp/alignment-cpp-declarations?view=msvc-170#alignof-and-alignas
+
+    // aligns : 여러 alignas 개 적용된 경우 결과 맞춤은
+    // 모든 alignas 값 중에서 가장 크며 적용된 형식의 자연 맞춤보다 작을 수 없습니다.
+    // https://learn.microsoft.com/ko-kr/cpp/cpp/alignas-specifier?view=msvc-170
 
     test1();
     test2();
